d4d24f4d5ec5e4aba760388517fb0e05ad21c2a7
theshatterstone <theshatterstone@protonmail.com>
2024-09-16 19:02:31 +0100

    Added Wallpaper

--- files/system/etc/skel/.config/qtile/scripts/change-to-theme2.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-cp $HOME/.config/qtile/config-theme2.py $HOME/.config/qtile/config.py
-
-cp $HOME/.config/starship-theme2.toml $HOME/.config/starship.toml
-
-cp $HOME/.config/fish/config-theme2.fish $HOME/.config/fish/config.fish
